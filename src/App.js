f7ac7a35df24332ab17ad51207702ce335b5cad3
anoop-rc <rc.anoop@gmail.com>
2021-11-17 10:06:55 +0530

    initial commit with api call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+/**
+ * TODO: check for city and temperature when conditionally rendering
+ * make the app look 🚳 dispaly other weather data you find usefull
+ * 
+ */
+import { useState } from 'react';
 import './App.css';
+import axios from './axios';
+
+const APIkey = 'f6e65c92c90cacc50feba18db079365a'
 
 function App() {
+  const [city, setCity] = useState('');
+  const [temperature, settemperature] = useState(null)
+
+  const getWeather = async () => {
+    const response = await axios.get(`/weather?q=${city}&appid=${APIkey}&units=metric`)
+
+    settemperature(response.data.main.temp)
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        temperature && (`Hello ${city} the temperature is ${temperature}`)
+      }
+      <h1>Hello World</h1>
+      <input
+        placeholder={'Type the city name'}
+        value={city}
+        onChange={event => setCity(event.target.value)}
+      />
+      <button onClick={() => getWeather()}>Get Weather</button>
     </div>
   );
 }
